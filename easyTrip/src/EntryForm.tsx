e39bd865fe0f0d5aca35ce7a0fdc42cac0b58f49
Rails Udemy Course <14225425@life.hkbu.edu.hk>
2020-03-15 16:23:42 +0800

    Edit appbar

--- easyTrip/src/EntryForm.tsx
+++ easyTrip/src/EntryForm.tsx
@@ -1,26 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import {
-    Card, Button, CardHeader, CardBody,
+    Card, CardHeader, CardBody,
     CardTitle, CardText, Row, Col, Container
 } from "reactstrap";
-
-import './EntryForm.scss'
 import moment from "moment";
 import 'react-dates/initialize';
 import "react-dates/lib/css/_datepicker.css";
 import { DateRangePicker } from 'react-dates';
+import Select from 'react-select'
+import { Button } from '@material-ui/core';
+import './EntryForm.scss'
+
+import Responsive from "react-responsive";
 
 
 export function EntryForm() {
 
-
-    const { handleSubmit, register } = useForm();
-
-    const onSubmit = (values: any) => {
-        console.log(values);
-    }
-
+    // react-dates setting
     const [startDate, setStartDate] = useState<moment.Moment | null>(null);
     const [endDate, setEndDate] = useState<moment.Moment | null>(null);
     const [focusedInput, setFocusedInput] = useState<"startDate" | "endDate" | null>(null);
@@ -29,8 +26,55 @@ export function EntryForm() {
         setEndDate(endDate);
     };
 
-    const startDateValue = startDate ? `${startDate.toDate().getFullYear()}-${startDate.toDate().getMonth() + 1}-${startDate.toDate().getDate()}` : ""
-    const endDateValue = endDate ? `${endDate.toDate().getFullYear()}-${endDate.toDate().getMonth() + 1}-${endDate.toDate().getDate()}` : ""
+
+    let startDateValue = startDate ? `${startDate.toDate().getFullYear()}-${startDate.toDate().getMonth() + 1}-${startDate.toDate().getDate()}` : ""
+    let endDateValue = endDate ? `${endDate.toDate().getFullYear()}-${endDate.toDate().getMonth() + 1}-${endDate.toDate().getDate()}` : ""
+
+
+    const { handleSubmit, register, setValue } = useForm();
+
+    // set up a custom register using the useEffect Hook and update the value via setValue.
+    setValue("trip-start-date", startDateValue)
+    setValue("trip-end-date", endDateValue)
+
+
+    useEffect(() => {
+        register({ name: "reactSelect" }); // custom register antd input
+        register({ name: "trip-start-date" }); // custom register react-select 
+        register({ name: "trip-end-date" }); // custom register antd input
+    }, [register])
+
+    const onSubmit = (values: any) => {
+        console.log(values);
+    }
+
+    // react-select setting
+    const [values, setReactSelectValue] = useState({ selectedOption: [] });
+
+    const handleSignleChange = (selectedOption: any) => {
+        setValue("reactSelect", selectedOption.value);
+        setReactSelectValue({ selectedOption });
+        console.log(selectedOption);
+    }
+
+    // console.log({ startDateValue: startDateValue });
+    // console.log({ endDateValue: endDateValue });
+
+
+    const options = [
+        { value: 'hongkong', label: 'Hong Kong' },
+        { value: 'london', label: 'London' },
+        { value: 'singapore', label: 'Singapore' }
+    ]
+
+
+    const customStyles = {
+        control: (base: any, state: any) => ({
+            ...base,
+            height: '48px',
+            borderRadius: "0"
+        }),
+    };
 
     return (
         <Container className="app">
@@ -44,35 +88,58 @@ export function EntryForm() {
                 <CardHeader className="form-header">Itinerary Planner</CardHeader>
                 <CardBody>
                     <form className="form" onSubmit={handleSubmit(onSubmit)}>
-                        <input className="city" type="city" name="city" placeholder="Enter destination (Country, Region, City)"
-                            ref={register({
-                                required: 'Required'
-                            })} />
+                        <Select
+                            className="city"
+                            value={values.selectedOption}
+                            options={options}
+                            onChange={handleSignleChange}
+                            isClearable
+                            isSearchable
+                            placeholder="Enter destination (Country, Region, City)"
+
+                            styles={customStyles}
+                        />
                         <Row>
-                            <Col>
-                                <DateRangePicker
-                                    startDate={startDate}
-                                    startDateId="trip-start-date"
-                                    endDate={endDate}
-                                    endDateId="trip-end-date"
-                                    onDatesChange={handleDatesChange}
-                                    focusedInput={focusedInput}
-                                    onFocusChange={focusedInput => setFocusedInput(focusedInput)}
-                                    displayFormat="YYYY-MM-DD"
-                                />
+                            <Col className="date-picker">
+                                <Responsive
+                                    maxWidth={767}>
+                                    <DateRangePicker
+                                        startDate={startDate}
+                                        startDateId="trip-start-date"
+                                        endDate={endDate}
+                                        endDateId="trip-end-date"
+                                        onDatesChange={handleDatesChange}
+                                        focusedInput={focusedInput}
+                                        onFocusChange={focusedInput => setFocusedInput(focusedInput)}
+                                        displayFormat="ddd MM/DD"
+                                        withPortal={true}
+                                        numberOfMonths={1}
+                                        required
+                                    />
+                                </Responsive>
+                                <Responsive
+                                    minWidth={768}>
+                                    <DateRangePicker
+                                        startDate={startDate}
+                                        startDateId="trip-start-date"
+                                        endDate={endDate}
+                                        endDateId="trip-end-date"
+                                        onDatesChange={handleDatesChange}
+                                        focusedInput={focusedInput}
+                                        onFocusChange={focusedInput => setFocusedInput(focusedInput)}
+                                        displayFormat="ddd MM/DD"
+                                        withPortal={true}
+                                        showClearDates
+                                        required
+                                    />
+                                </Responsive>
                             </Col>
                         </Row>
                         <Row className="btn-header">
-                            <Col >
-                                <Button className="btn" type="submit">Start Planning</Button>
+                            <Col className="btn">
+                                <Button variant="contained" type="submit">Start Planning</Button>
                             </Col>
                         </Row>
-                        <input className="date" type="text" name="startDate" value={123} />
-                        <input className="date" type="text" name="endDate" value={endDateValue}/>
-                        {/* <input className="city" type="city1" name="city1" placeholder="Enter destination (Country, Region, City)"
-                            ref={register({
-                                required: 'Required'
-                            })} /> */}
                     </form>
                 </CardBody>
             </Card>
--- easyTrip/src/MainNav.tsx
+++ easyTrip/src/MainNav.tsx
@@ -1,32 +1,128 @@
 import React from 'react';
 import './MainNav.scss';
-
-import {
-    Navbar,
-    Input
-} from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, IconButton, Typography, Button, Theme, makeStyles, createStyles, InputBase, fade, Grid } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+import Responsive from "react-responsive";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 
-export interface INavbarProps {
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            light: '#424242',
+            main: '#212121'
+        }
+    },
+});
 
 
-}
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 1,
+            display: 'none',
+            [theme.breakpoints.up('sm')]: {
+                display: 'block',
+            },
+        },
+        search: {
+            position: 'relative',
+            borderRadius: theme.shape.borderRadius,
+            backgroundColor: fade(theme.palette.common.white, 0.15),
+            '&:hover': {
+                backgroundColor: fade(theme.palette.common.white, 0.25),
+            },
+            marginLeft: 0,
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                marginLeft: theme.spacing(1),
+                width: 'auto',
+            },
+        },
+        searchIcon: {
+            width: theme.spacing(7),
+            height: '100%',
+            position: 'absolute',
+            pointerEvents: 'none',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        inputRoot: {
+            color: 'inherit',
+        },
+        inputInput: {
+            padding: theme.spacing(1, 1, 1, 7),
+            transition: theme.transitions.create('width'),
+            width: '100%',
+            [theme.breakpoints.up('sm')]: {
+                width: 120,
+                '&:focus': {
+                    width: 200,
+                },
+            },
+        },
+        rightToolbar: {
+            marginLeft: 'auto',
+            marginRight: -12,
+        },
+    }),
+);
 
-export function MainNav(props: INavbarProps) {
-    // const [isOpen, setIsOpen] = useState(false);
-
-    // const toggle = () => setIsOpen(!isOpen);
+export function MainNav() {
+    const classes = useStyles();
 
     return (
-        <div className="mynav">
-            <Navbar className="mynavbar" color="dark">
-                <h3>
-                    <Link to="/">EasyTrip</Link>
-                </h3>
-                <Input className="search-bar" type="search" placeholder="search"></Input>
-                <Link to="/login" className="login" >Login</Link>
-            </Navbar>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+
+                        <Typography variant="h6" className={classes.title}>
+                            EasyTrip
+                        </Typography>
+
+                        <Responsive minWidth={768}>
+                            <div className={classes.search}>
+                                <div className={classes.searchIcon}>
+                                    <SearchIcon />
+                                </div>
+                                <InputBase
+                                    placeholder="Search…"
+                                    classes={{
+                                        root: classes.inputRoot,
+                                        input: classes.inputInput,
+                                    }}
+                                    inputProps={{ 'aria-label': 'search' }}
+                                />
+                            </div>
+                            <Button color="inherit"><Link to="/login"></Link> Login</Button>
+                        </Responsive>
+                        <Responsive maxWidth={767} >
+                             <section>
+                                <IconButton edge="start" className={classes.menuButton} aria-label="menu">
+                                    <MenuIcon />
+                                </IconButton>
+                            </section>            
+                            <section className={classes.rightToolbar}>
+                                <IconButton aria-label="search" color="inherit">
+                                    <SearchIcon />
+                                </IconButton>
+                                <Button color="inherit"><Link to="/login">Login</Link> </Button>
+                            </section>
+                        </Responsive>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        </ThemeProvider>
     );
 }
